57aa30dfaa5774ed5dc17585bfd3b5be86122d62
luisbazdev <luisbazd@gmail.com>
2023-08-10 14:25:18 -0400

    Export authentication middlewares from entry file

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 import { Pigeon } from "./src/pigeon";
 import { MySQLConnection, MongoDBConnection } from "./src/database";
+import { basicHTTPAuthentication, JWTAuthentication } from "./src/auth";
 
-export { Pigeon, MySQLConnection, MongoDBConnection };
-export * from './src/interfaces';
+export {
+  Pigeon,
+  basicHTTPAuthentication,
+  JWTAuthentication,
+  MySQLConnection,
+  MongoDBConnection,
+};
+export * from "./src/interfaces";
