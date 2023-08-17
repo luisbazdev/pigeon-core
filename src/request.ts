d30c13e555932d527c12c4dd7a700e97989b2afb
luisbazdev <luisbazd@gmail.com>
2023-08-17 16:40:24 -0400

    Make 'IncomingMessage' properties of type 'any'

--- src/request.ts
+++ src/request.ts
@@ -1,14 +1,13 @@
 import { IncomingMessage } from "node:http";
-import { JWTLogInCredentials, JWTSignUpCredentials } from "./interfaces";
 
 declare module "http" {
   interface IncomingMessage {
     get: (header: string) => any;
     body: any;
-    cookies: object;
-    query: object;
-    params: object;
-    user: object;
+    cookies: any;
+    query: any;
+    params: any;
+    user: any;
   }
 }
 
